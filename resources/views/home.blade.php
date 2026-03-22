1095c555ceb0a06bc1016317cb197ef1c5a6fd69
zohrasebai <zohra.sebai@univ-bba.dz>
2026-03-22 23:30:38 +0100

    Mise à jour site

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,18 +4,11 @@
 @section('title', 'Qualipro - Home')
 
 @section('content')
-	<div class="bg-secondery color-white" id="scroll" style="display: inline;"><i class="fa fa-angle-up"></i></div>
-
     @include('partials.header')
-    @include('partials.slider')
-    @include('partials.about')
-    @include('partials.video')
-    @include('partials.core')
-    @include('partials.causes')
-    @include('partials.help')
-    @include('partials.offer')
-    @include('partials.partners')
-    
+    @include('partials.slider')    {{-- La vidéo --}}
+    @include('partials.about')     {{-- Qui sommes-nous --}}
+    @include('partials.services')  {{-- Nos services --}}
+    @include('partials.references') {{-- NOUVEAU : Créez ce fichier pour vos logos clients --}}
+    @include('partials.contact')   {{-- Contact --}}
     @include('partials.footer')
-
 @endsection
---
+++ resources/views/partials/references.blade.php
@@ -0,0 +1,17 @@
+{{-- Section Nos Références / Certifications --}}
+<section id="references" class="py-50 bg-white">
+    <div class="container text-center">
+        <h2 class="mb-30" style="color: #002e5b;">Nos Références & Certifications</h2>
+        <div class="row align-items-center justify-content-center">
+            {{-- Exemple de Logo 1 --}}
+            <div class="col-md-2 col-6 mb-20">
+                <img src="{{ asset('assets/images/references/logo_iso.png') }}" alt="ISO" class="img-fluid" style="filter: grayscale(100%); opacity: 0.7;">
+            </div>
+            {{-- Exemple de Logo 2 --}}
+            <div class="col-md-2 col-6 mb-20">
+                <img src="{{ asset('assets/images/references/logo_haccp.png') }}" alt="HACCP" class="img-fluid" style="filter: grayscale(100%); opacity: 0.7;">
+            </div>
+            {{-- Ajoutez d'autres logos ici de la même manière --}}
+        </div>
+    </div>
+</section>
