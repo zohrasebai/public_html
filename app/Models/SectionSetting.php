5f286403ddffa657d8e778cffa01b3521139fecf
zohrasebai <zohra.sebai@univ-bba.dz>
2026-03-23 14:46:15 +0000

    mdf9

--- app/Models/SectionSetting.php
+++ app/Models/SectionSetting.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class SectionSetting extends Model
 {
-    //
+    // Permet d'insérer des données dans ces colonnes via l'admin
+    protected $fillable = [
+        'section_name', 
+        'title_fr', 
+        'subtitle_fr', 
+        'desc_fr'
+    ];
 }
