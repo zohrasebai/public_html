60e45fcd35fce299a61e451021fdf4a2a291111d
zohrasebai <zohra.sebai@univ-bba.dz>
2026-03-23 13:58:42 +0000

    mdf3

--- resources/views/partials/slider.blade.php
+++ resources/views/partials/slider.blade.php
@@ -1,7 +1,7 @@
 <section id="home" class="relative w-full h-screen overflow-hidden">
     {{-- La Vidéo en arrière-plan --}}
     <video autoplay muted loop playsinline class="absolute top-0 left-0 w-full h-full object-cover">
-        <source src="{{ asset('assets/video/votre_equipe.mp4') }}" type="video/mp4">
+        <source src="{{ asset('assets/video/funfactSoul.mp4') }}" type="video/mp4">
         {{-- Image de secours si la vidéo ne charge pas --}}
         <img src="{{ asset('assets/images/slider/bg1.jpg') }}" alt="Equipe QualiPro">
     </video>
