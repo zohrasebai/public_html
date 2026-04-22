40983fd8955a1e120f005bacbfccc79099762245
zohrasebai <zohra.sebai@univ-bba.dz>
2026-04-22 12:29:08 +0000

    Toutes les modifications finales

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,7 @@
     @include('partials.header')
     @include('partials.slider')    {{-- La vidéo --}}
     @include('partials.about')     {{-- Qui sommes-nous --}}
-    @include('partials.services')  {{-- Nos services --}}
+    @include('partials.causes')  {{-- Nos services --}}
     @include('partials.references') {{-- NOUVEAU : Créez ce fichier pour vos logos clients --}}
     @include('partials.contact')   {{-- Contact --}}
     @include('partials.footer')
